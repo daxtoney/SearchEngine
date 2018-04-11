3e7d8fdc3f284e92d56c903cdb8e4883dd29b49e
ramirezlg1 <ramirezlg1@gcc.edu>
2018-04-10 20:33:44 -0700

    DictionaryQuery ready
    ready to be tested

--- Query.scala
+++ Query.scala
@@ -14,3 +14,58 @@ class Query (strs: Seq[String]) extends Weighted[String] {
 		wordAndWeight = strs.map((_, 1.0))
 	}
 }
+
+class DictionaryQuery(strs: Seq[String]) extends Query(strs: Seq[String]) {
+	var goodDictionary: Seq[String] = Seq[String]()
+	var badDictionary: Seq[String] = Seq[String]()
+
+	var superWordAndWeight: Seq[(String, Double)] = Seq[(String, Double)]()
+	var wordAndWeight: Seq[(String, Double)] = Seq[(String, Double)]()
+
+	def loadGood(){
+		val bufferedSource = io.Source.fromFile("Dictionary.csv")
+		for (line <- bufferedSource.getLines){
+			val cols = line.split(",")
+			val words = cols(0).split(" ")
+			if (cols != Nil){
+				for (w <- words){
+					goodDictionary :+ w
+				}
+			}
+		}
+	}
+
+	def loadBad(){
+		val bufferedSource = io.Source.fromFile("Anti-dictionary.csv")
+		for (line <- bufferedSource.getLines){
+			val cols = line.split(",")
+			for (c <- cols){
+				badDictionary :+ c
+			}
+		}
+	}
+
+	override def assignWeights(): Unit = {
+		
+		for (s <- superWordAndWeight; g <- goodDictionary){
+			if (s._1 == g){
+				wordAndWeight :+ (s._1, 10.0)
+			}
+		}
+		
+		for (s <- superWordAndWeight; b <- badDictionary){
+			if (s._1 == b){
+				wordAndWeight :+ (s._1, 0.0)
+			}
+		}
+
+		for (s <- strs; if (!goodDictionary.contains(s) && !badDictionary.contains(s))){
+			wordAndWeight :+ (s, 1.0)
+		}
+
+		/*val notCovered = strs.filterNot( _ => goodDictionary.contains(_) && badDictionary.contains(_))
+		for (n <- notCovered){
+			wordAndWeight :+ (n, 1.0)
+		}*/
+	}
+}
